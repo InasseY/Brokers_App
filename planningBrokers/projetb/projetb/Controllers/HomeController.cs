6b4a186ea1998eef2fcb68b8f8eff031c8f711d8
InasseY <inasse.youssouf@gmail.com>
2021-04-08 16:42:36 +0200

    brokers planning add brokers

---
+++ planningBrokers/projetb/projetb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using projetb.Models;
+using System.Web.Mvc;
+using projetb.Models.data; 
+
+namespace projetb.Controllers
+{
+    public class HomeController : Controller
+    {
+        agendaEntities db = new agendaEntities(); 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListBrokers()
+        {
+            return View(db.Brokers.ToList());
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
