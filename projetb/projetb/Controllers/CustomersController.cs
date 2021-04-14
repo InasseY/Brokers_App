9551e1f63c99a84b8545d02b7ba025f185a29790
InasseY <inasse.youssouf@gmail.com>
2021-04-14 13:01:22 +0200

    ajout clients + confirmation delete

--- projetb/projetb/Controllers/CustomersController.cs
+++ projetb/projetb/Controllers/CustomersController.cs
@@ -32,5 +32,45 @@ namespace projetb.Controllers
             }
             return View(customer);
         }
+        public ActionResult CustomersList()//afficher la liste des clients
+        {
+            var customer = dbj.Customers.ToList();
+            return View(customer);
+        }
+       
+        public ActionResult Delete(int id) //supprimer un client de la liste
+        {
+            var customer = dbj.Customers.Where(model => model.idCustomer == id).First();
+            dbj.Customers.Remove(customer);
+            dbj.SaveChanges();
+
+            var list = dbj.Customers.ToList();
+            return View("CustomersList", list);
+        }
+        public ActionResult profilC(int id) //afficher les informations clients
+        {
+            var custom = dbj.Customers.Where(s => s.idCustomer == id).First();
+            return View(custom);
+        }
+        public ActionResult editC(int id) //afficher les informations du clients à modifier
+        {
+            var customer = dbj.Customers.Where(s => s.idCustomer == id).First();
+            return View(customer);
+        }
+        [HttpPost]
+        public ActionResult editC(Customers cust) //modifier et enregistrer les nouvelles données clients
+        {
+            if (ModelState.IsValid)
+            {
+                if (TryUpdateModel(cust))
+                {
+                    dbj.Entry(cust).State = EntityState.Modified;
+                    dbj.SaveChanges();
+                    TempData["SuccessMessage"] = "Yeah c'est modifié !";
+                    return RedirectToAction("CustomersList");
+                }
+            }
+            return View(cust);
+        }
     }
 }
